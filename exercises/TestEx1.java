d2e4885c1d9d2a44dde8539de60f02a115acfe4d
ShadowBeast <ShadovvBeast@gmail.com>
2020-05-12 16:30:42 +0300

    fix TestEx1 weird bug

--- exercises/TestEx1.java
+++ exercises/TestEx1.java
@@ -3,29 +3,29 @@ package com;
 import junit.framework.TestCase;
 import org.junit.Test;
 import static com.DateCalculatorTestSetup.*;
+import java.time.LocalDate;
 import com.DateCalculator;
 import static org.junit.Assert.assertEquals;
 
-public class TestEx1 extends TestCase {
+public class TestEx1 {
 
-    @Test
-    public void DateStringTest() {
-        for (Date d : data) {
-            assertEquals(d.toString(), DateCalculator.DateString(d.Year, d.Month, d.Day));
+     public void DateStringTest() {
+            for (Date d : data) {
+                assertEquals(d.toString(), DateCalculator.DateString(d.Year, d.Month, d.Day));
+            }
+
+            for (Date d: invalidData) {
+                assertEquals("INVALID_DATE", DateCalculator.DateString(d.Year, d.Month, d.Day));
+                assertEquals("INVALID_DATE", DateCalculator.DateString(d.Year, d.Month, d.Day).toString());
+            }
         }
 
-        for (Date d: invalidData) {
-            assertEquals("INVALID_DATE", DateCalculator.DateString(d.Year, d.Month, d.Day));
-            assertEquals("INVALID_DATE", DateCalculator.DateString(d.Year, d.Month, d.Day).toString());
-        }
-    }
 
-
-    @Test
-    public void DateStringTestInvalidDates() {
-        for (Date d: invalidData) {
-            assertEquals("INVALID_DATE", DateCalculator.DateString(d.Year, d.Month, d.Day));
-            assertEquals("INVALID_DATE", DateCalculator.DateString(d.Year, d.Month, d.Day).toString());
+        @Test
+        public void DateStringTestInvalidDates() {
+            for (Date d: invalidData) {
+                assertEquals("INVALID_DATE", DateCalculator.DateString(d.Year, d.Month, d.Day));
+                assertEquals("INVALID_DATE", DateCalculator.DateString(d.Year, d.Month, d.Day).toString());
+            }
         }
-    }
 }
