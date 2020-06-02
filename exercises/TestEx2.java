80cb0dc02d4c69c9199158d23ad53f92c99e66b8
Kfir Yehezkel <kfir.ye@gmail.com>
2020-06-02 13:31:50 +0300

    improved DateDiff messaging.

--- exercises/TestEx2.java
+++ exercises/TestEx2.java
@@ -41,16 +41,52 @@ public class TestEx2 {
     @Test
     public void testDateDiff() {
         for (Date d: invalidData) {
-            assertEquals(Integer.MAX_VALUE, DateCalculator.DateDiff(d.Year, d.Month, d.Day, 1991, 4, 2));
-            assertEquals(Integer.MAX_VALUE, DateCalculator.DateDiff(1991, 4, 2, d.Year, d.Month, d.Day));
-            assertEquals(Integer.MAX_VALUE, DateCalculator.DateDiff(d.toString(), new Date(1991, 4, 2).toString()));
-            assertEquals(Integer.MAX_VALUE, DateCalculator.DateDiff(new Date(1991, 4, 2).toString(), d.toString()));
+            int actual_result;
+            actual_result = DateCalculator.DateDiff(d.Year, d.Month, d.Day, 1991, 4, 2);
+            int expected_result = Integer.MAX_VALUE;
+            assertEquals(String.format("DateDiffDateDiff Function should return %d but it returned %d",
+                    d.Year, d.Month, d.Day, expected_result, actual_result),
+                    expected_result, actual_result);
+
+            actual_result = DateCalculator.DateDiff(1991, 4, 2, d.Year, d.Month, d.Day);
+            int expected_result = Integer.MAX_VALUE;
+            assertEquals(String.format("DateDiff(1991, 4, 2, d.Year, d.Month, d.Day) Function should return %d but it returned %d",
+                    d.Year, d.Month, d.Day, expected_result, actual_result),
+                    expected_result, actual_result);
+
+            String par1 = d.toString();
+            String par2 = new Date(1991, 4, 2).toString();
+            actual_result = DateCalculator.DateDiff(par1, par2);
+            assertEquals(String.format("DateDiff(%s, %s) Function should return %d but it returned %d",
+                    par1, par2, expected_result, actual_result),
+                expected_result, actual_result);
+
+            actual_result = DateCalculator.DateDiff(par2, par1);
+            assertEquals(String.format("DateDiff(%s, %s) Function should return %d but it returned %d",
+                    par2, par1, expected_result, actual_result),
+                    expected_result, actual_result);
         }
 
         for (Date d: data) {
+            int actual_result;
+            int expected_result;
+            LocalDate calc_day;
+
             LocalDate today = LocalDate.of(d.Year, d.Month, d.Day);
-            LocalDate yesterday =  today.minusDays(1);
-            assertEquals(yesterday.format(foramtter), DateCalculator.Yesterday(d.Year, d.Month, d.Day));
+            int days_period = randGenerator.nextInt(5000);
+
+            actual_result = DateCalculator.DateAdd(d.toString(), days_period));
+            calc_day =  today.plusDays(days_period);
+
+            assertEquals(String.format("DateDiff(%s,%s) Function should return %d but it returned %d",
+                    today.format(foramtter), calc_day.format(formatter), days_period, actual_result),
+                    days_period, actual_result);
+
+            actual_result = DateCalculator.DateAdd(d.toString(), -days_period));
+            calc_day =  today.minusDays(days_period);
+            assertEquals(String.format("DateDiff(%s,%s) Function should return %d but it returned %d",
+                    today.format(foramtter), calc_day.format(formatter), -days_period, actual_result),
+                    -days_period, actual_result);
         }
     }
 }
